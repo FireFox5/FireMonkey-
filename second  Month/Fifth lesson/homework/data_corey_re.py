fe929f11f221b60906069d4d4b98410afc6a3c5c
Sultanmurat <sultanmuratnaspekov@gmail.com>
2021-09-03 23:11:26 +0600

    '123'

--- second  Month/Fifth lesson/homework/data_corey_re.py
+++ second  Month/Fifth lesson/homework/data_corey_re.py
@@ -1,2 +1,23 @@
 import re
-open()
+
+something = open("src/data_corey.txt", 'r')
+line_count = 0
+line_numbers = 0
+kak=0
+for line in something:
+
+    if line != "\n":
+
+
+        kak += 1
+        if (kak == 2):
+            if (line.find('7', 11, 12) == 11):
+                line_numbers += 1
+        if (kak == 4):
+            kak = 0
+            line_count += 1
+
+
+something.close()
+print(line_count)
+print(line_numbers)
---
+++ second  Month/seventh lesson 02.09.2021/7.1.py
@@ -0,0 +1,10 @@
+#algoritms  Алгоритмы
+numbers=[2,4,6,7,11,44,12,56,23,25,67]
+res=4 in numbers
+print(res)
+
+for i in numbers:
+    if i==2:
+        print('$$$',[])
+        break
+#hacker rank
